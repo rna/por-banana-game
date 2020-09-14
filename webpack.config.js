5b743d40af9b576ef19bc7a5bc4e6c9b50fcbccf
Ramesh <rameshnaidu.allu@gmail.com>
2020-09-14 10:08:56 +0530

    add webpack setup with config, npm modules, src files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname,'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })],
+}
