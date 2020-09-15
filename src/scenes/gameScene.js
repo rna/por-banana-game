f47f25a77c5cdc7c33bfdff6ef17c586c4e5fa07
Ramesh <rameshnaidu.allu@gmail.com>
2020-09-15 09:23:33 +0530

    remove clouds and add ground in game scene

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -1,9 +1,7 @@
 import Phaser from 'phaser';
 import sky from '../assets/Sky.png';
-import cloud1 from '../assets/cloud1.png';
-import cloud2 from '../assets/cloud2.png';
-import cloud3 from '../assets/cloud3.png';
 import shrubs from '../assets/shrubs.png';
+import ground from '../assets/ground.png';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -12,36 +10,21 @@ export default class GameScene extends Phaser.Scene {
 
   preload() {
     this.load.image('background', sky);
-    this.load.image('cloud1', cloud1);
-    this.load.image('cloud2', cloud2);
-    this.load.image('cloud3', cloud3);
     this.load.image('trees', shrubs);
+    this.load.image('ground', ground);
   }
 
   create() {
     this.add.image(400, 300, 'background');
-    this.cloud1 = this.add.image(300, 80, 'cloud1');
-    this.cloud2 = this.add.image(500, 90, 'cloud2');
-    this.cloud3 = this.add.image(700, 70, 'cloud3');
-    this.tree = this.add.tileSprite(0, 400, 0, 0, 'trees');
+    this.tree = this.add.tileSprite(0, 0, this.game.config.width, 0, 'trees');
+    this.tree.setOrigin(0, 0);
+    this.tree.setScrollFactor(0);
+    this.ground = this.add.tileSprite(0, 0, this.game.config.width, 96, 'ground');
+    this.ground.setOrigin(0, 0);
+    this.ground.setScrollFactor(0);
+    this.ground.y = 500;
   }
 
   update() {
-    this.cloud1.x -= 0.3;
-    if (this.cloud1.x < -350) {
-      this.cloud1.x = 1150;
-    }
-    this.cloud2.x -= 0.2;
-    if (this.cloud2.x < -350) {
-      this.cloud2.x = 1150;
-    }
-    this.cloud3.x -= 0.1;
-    if (this.cloud3.x < -350) {
-      this.cloud3.x = 1150;
-    }
-    this.tree.x -= 5;
-    if (this.tree.x < 0) {
-      this.tree.x = 800;
-    }
   }
 }
